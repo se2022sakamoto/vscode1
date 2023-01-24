1e1581f03942100af5ba02b50894faadb2bf5bdb
sakamoto <r202103321xr@jindai.jp>
2023-01-25 03:52:47 +0900

    using char

--- vscode.c
+++ vscode.c
@@ -1,3 +1,12 @@
 #include <stdio.h>
-int main(void){ 
-printf("hello world") ;}
+#include <string.h>
+
+int main(void)
+{
+	size_t len;
+	len = strlen("hello world");
+	printf("hello world\n");
+    printf("length:%d \n", len);
+
+	return 0;
+}
