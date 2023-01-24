1a5fd7ec12a2357ca9ffe3208aea50b5d3a265e5
sakamoto <r202103321xr@jindai.jp>
2023-01-25 03:54:15 +0900

    using integer

--- vscode.c
+++ vscode.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-#include <string.h>
-
+i=0;
 int main(void)
 {
-	size_t len;
-	len = strlen("hello world");
-	printf("hello world\n");
-    printf("length:%d \n", len);
+    i=5;
+	printf("hello %d\n",i);
 
 	return 0;
 }
